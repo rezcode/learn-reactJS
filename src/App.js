49c6ae0826656ef4d5a3367108c73431517cbd25
System Administrator <root@192.168.0.109>
2021-01-09 14:52:41 +0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+ );
 }
 
 export default App;
